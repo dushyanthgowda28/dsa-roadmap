284f5dc8544509d479cdf6d580d3973f8bd46647
dushyanthgowda28 <pantheralio1998@gmail.com>
2026-05-18 00:05:15 +0530

    minor changes

---
+++ src/arrays/stockbuyandsell/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,35 @@
+package arrays.stockbuyandsell;
+
+public class BestTimeToBuyAndSellStock {
+
+    public static int getMaximumProfit(int[] arr) {
+
+        if (arr == null || arr.length < 2) {
+            throw new IllegalArgumentException("Invalid array");
+        }
+
+        int buyPrice = arr[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+
+            // Calculate profit for current day
+            int currentProfit = arr[i] - buyPrice;
+
+            // Update maximum profit
+            maxProfit = Math.max(maxProfit, currentProfit);
+
+            // Update minimum buying price
+            buyPrice = Math.min(buyPrice, arr[i]);
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {7, 10, 1, 3, 6, 9, 2};
+
+        System.out.println("Maximum profit is " + getMaximumProfit(arr));
+    }
+}
---
+++ src/arrays/stockbuyandsell/BestTimeToBuyAndSellStockBruteForce.java
@@ -0,0 +1,35 @@
+package arrays.stockbuyandsell;
+
+public class BestTimeToBuyAndSellStockBruteForce {
+
+    public static int getMaximumProfit(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            throw new IllegalArgumentException("Invalid array");
+        }
+
+        int len = arr.length;
+        int maxProfit = 0;
+
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                maxProfit = Math.max(maxProfit, arr[j] - arr[i]);
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[] {7, 10, 1, 3, 6, 9, 2};
+        System.out.println("Maximum profit is " + getMaximumProfit(arr));
+    }
+}
+
+/*
+ * Time Complexity:
+ * Worst Case  : O(n^2)  -> when we check all possible pairs
+ * Best Case   : O(1)    -> when first pair matches target
+ * Average Case: O(n^2)
+ *
+ * Space Complexity:
+ * O(1) -> no extra data structure used (only variables i, j, len)
+ */
